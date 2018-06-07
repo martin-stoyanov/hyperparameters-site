7d9ec0da5d3dfa618d117da8a86c87b7b5b13fda
@martin.a.stoyanov <martin.a.stoyanov@gmail.com>
2018-06-07 15:47:01 -0400

    changed nsamples

--- components/utils/spaceToArray.js
+++ components/utils/spaceToArray.js
@@ -2,7 +2,7 @@ import RandomState from '../../src/utils/RandomState';
 // import RandomState from 'hyperparameters/src/utils/RandomState';
 import { sample } from '../../src/base/stochastic';
 
-export default (space, NSamples = 2000, seed = undefined) => {
+export default (space, NSamples = 200, seed = undefined) => {
   const arr = [];
   const rng = new RandomState(seed);
   for (let i = 0; i < NSamples; i += 1) {
