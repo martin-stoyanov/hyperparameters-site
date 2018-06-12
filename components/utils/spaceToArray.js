46b92cebf65c00ae11314a0f2831fa17248b2b8a
@martin.a.stoyanov <martin.a.stoyanov@gmail.com>
2018-06-12 10:17:55 -0400

    put NSamples back to 400

--- components/utils/spaceToArray.js
+++ components/utils/spaceToArray.js
@@ -1,6 +1,6 @@
 import { RandomState, sample } from 'hyperparameters';
 
-export default (space, NSamples = 800, seed = undefined) => {
+export default (space, NSamples = 400, seed = undefined) => {
   const arr = [];
   const rng = new RandomState(seed);
   for (let i = 0; i < NSamples; i += 1) {
