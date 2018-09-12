5796f95b0a86424f3ef324c65b188f7530e60969
@martin.a.stoyanov <martin.a.stoyanov@gmail.com>
2018-09-12 14:07:17 -0400

    Added google analytics

--- components/Layout.js
+++ components/Layout.js
@@ -4,22 +4,39 @@ import Head from 'next/head';
 import Header from './Header';
 import Footer from './Footer';
 import theme from './utils/theme';
+import { initGA, logPageView } from './utils/analytics';
 
-const Layout = ({ title, children, description = 'hpjs is a Javascript library for hyperparameter optimization. It is based on the Python library Hyperopt ' }) => (
-  <React.Fragment>
-    <Head>
-      <title>{title}</title>
-      <title>hpjs: Hyperparameter Optimization for Javascript</title>
-      <meta name='description' content={description} />
-    </Head>
-    <Grommet theme={theme}>
-      <Header />
-      <Box pad={{ vertical: 'large' }}>
-        {children}
-      </Box>
-      <Footer />
-    </Grommet>
-  </React.Fragment>
-);
+class Layout extends React.Component {
+  componentDidMount() {
+    if (!window.GA_INITIALIZED) {
+      initGA();
+      window.GA_INITIALIZED = true;
+    }
+    logPageView();
+  }
+  render() {
+    const {
+      title,
+      children,
+      description = 'hpjs is a Javascript library for hyperparameter optimization. It is based on the Python library Hyperopt ',
+    } = this.props;
 
+    return (
+      <React.Fragment>
+        <Head>
+          <title>{title}</title>
+          <title>hpjs: Hyperparameter Optimization for Javascript</title>
+          <meta name='description' content={description} />
+        </Head>
+        <Grommet theme={theme}>
+          <Header />
+          <Box pad={{ vertical: 'large' }}>
+            {children}
+          </Box>
+          <Footer />
+        </Grommet>
+      </React.Fragment>
+    );
+  }
+}
 export default Layout;
