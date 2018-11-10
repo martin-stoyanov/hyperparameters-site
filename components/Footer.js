18ad1db46611629be2b1e22a3f9b81912a6c8d2e
@martin.a.stoyanov <martin.a.stoyanov@gmail.com>
2018-11-10 15:42:40 -0500

    fixed footer anchor

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => (
   <Box>
     <Box align='center' background='brand' pad='small' gap='xsmall'>
       <Box direction='row' gap='xsmall'>
-        <Anchor href='https://github.com/atanasster/hyperparameters' target='_blank'><Text weight='700'>hpjs</Text></Anchor>
+        <Anchor href='https://github.com/atanasster/hyperparameters' target='_blank'><Text weight={700}>hpjs</Text></Anchor>
       </Box>
       <Box>
         Copyright (c) {(new Date().getFullYear())} Atanas & Martin Stoyanov
