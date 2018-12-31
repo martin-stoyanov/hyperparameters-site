7d2be90c253d084f6b3a12995696ebe90bd2524e
@martin.a.stoyanov <martin.a.stoyanov@gmail.com>
2018-12-31 09:44:43 -0500

    comment changes to iris code

--- components/editor/tfjs_examples/iris.js
+++ components/editor/tfjs_examples/iris.js
@@ -1,5 +1,6 @@
 export default
 `
+// training function, called by the optimization function
 async function trainModel({ numLayers, optimizer }, { xTrain, yTrain, xTest, yTest }) {
   const optimizers = {
     sgd: tf.train.sgd,
@@ -13,7 +14,7 @@ async function trainModel({ numLayers, optimizer }, { xTrain, yTrain, xTest, yTe
   // adding random number of layers
   for (let i = 0; i < numLayers; i += 1) {
     model.add(tf.layers.dense({
-      inputShape: i === 0 ? [4] : [10], // if first layer
+      inputShape: i === 0 ? [4] : [10], // input shape 4 for 1st layer
       activation: i === numLayers - 1 ? 'softmax' : 'sigmoid',
       units: i === numLayers - 1 ? 3 : 10,
     }));
@@ -31,7 +32,7 @@ async function trainModel({ numLayers, optimizer }, { xTrain, yTrain, xTest, yTe
   return { model, h };
 }
 
-// fmin optmization function, retuns the loss and a STATUS_OK
+// fmin optmization function, retuns the accuracy, history, confusion matrix data, and a STATUS_OK
 async function modelOpt({ optimizer, numLayers }, { xTrain, yTrain, xTest, yTest }) {
   const { h, model } = await trainModel({ optimizer, numLayers }, { xTrain, yTrain, xTest, yTest });
   
@@ -48,14 +49,14 @@ async function modelOpt({ optimizer, numLayers }, { xTrain, yTrain, xTest, yTest
 }
 
 // hyperparameters search space
-// optmizer is a choice field
-// layers ia an integer value from 2 to 5 with a step of 1
+// optimizer is a choice field
+// layers is an integer value from 2 to 5 with a step of 1
 const space = {
   optimizer: hpjs.choice(['sgd', 'adam', 'adagrad', 'rmsprop']),
   numLayers: hpjs.quniform(2, 5, 1),
 };
-// Generate some synthetic data for training. (y = 2x - 1) and pass to fmin as parameters
-// data will be passed as a parameters to the fmin
+
+// Getting data to train, using the tensorflowjs iris example's data
 const [xTrain, yTrain, xTest, yTest] = data.getIrisData(0.15);
 
 return await hpjs.fmin(
