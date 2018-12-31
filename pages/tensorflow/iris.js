cf5e4c529be9f9069de1c9552b61967985e6649a
@martin.a.stoyanov <martin.a.stoyanov@gmail.com>
2018-12-31 13:28:21 -0500

    better descriptions for tiny and iris model

--- pages/tensorflow/iris.js
+++ pages/tensorflow/iris.js
@@ -5,7 +5,11 @@ import * as data from '../../data/iris';
 export default () => (
   <TensorflowExample
     name='Iris tensorflow model'
-    description='Find best optimizer and number of layers for the the iris dataset.'
+    description='<br /><div>Find best optimizer and number of layers for the the iris dataset.
+      <ul><li>To change seed: on line 63: replace RandomState(54321) with any number
+      Or, to get random results every time you run, remove the whole argument.</li>
+      <li>To change the # of epochs: line 28</li>
+      <li>To change # of trials: replace the 6 on line 62 with # of trials</li></ul></div>'
     code={irisTensorflowModel}
     data={data}
   />
--- pages/tensorflow/tiny.js
+++ pages/tensorflow/tiny.js
@@ -4,7 +4,12 @@ import tinyTensorflowModel from '../../components/editor/tfjs_examples/tiny';
 export default () => (
   <TensorflowExample
     name='Tiny tensorflow model'
-    description='Find best optimizer and number of epochs for the "tiny" tensorflow.js sample.'
+    description='<br />Find best optimizer and number of epochs for the "tiny" tensorflow.js sample.
+    <ul>
+      <li>To change seed: on line 34: replace RandomState(54321) with any number
+      Or, to get random results every time you run, remove the whole argument.</li>
+      <li>To change # of trials: replace the 10 on line 33 with # of trials</li>
+    </ul>'
     code={tinyTensorflowModel}
   />
 );
