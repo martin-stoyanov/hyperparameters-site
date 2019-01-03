478b710e18e6d51413aeea6a70c5d5f6b6e52c37
@martin.a.stoyanov <martin.a.stoyanov@gmail.com>
2019-01-02 20:20:24 -0500

    added link to blog for iris example

--- pages/tensorflow/iris.js
+++ pages/tensorflow/iris.js
@@ -9,7 +9,9 @@ export default () => (
       <ul><li>To change seed: on line 63: replace RandomState(54321) with any number
       Or, to get random results every time you run, remove the whole argument.</li>
       <li>To change the # of epochs: line 28</li>
-      <li>To change # of trials: replace the 6 on line 62 with # of trials</li></ul></div>'
+      <li>To change # of trials: replace the 6 on line 62 with # of trials</li></ul>
+      <a href = https://medium.com/@martin_stoyanov/hyperparameter-optimization-for-the-iris-dataset-in-javascript-81f81809275e?source=your_stories_page target="_blank">Blog post</a> on this topic
+      </div>'
     code={irisTensorflowModel}
     data={data}
   />
