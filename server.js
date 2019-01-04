9152a5925fa9e96bfbea7c1c41c82b569758afd9
@martin.a.stoyanov <martin.a.stoyanov@gmail.com>
2019-01-04 12:01:53 -0500

    changed start script and port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const nextjs = require('next')
 const compression = require('compression')
     
+const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production'
 const app = nextjs({ dev })
 const handle = app.getRequestHandler()
@@ -17,9 +18,9 @@ app.prepare()
     return handle(req, res)
   })
     
-  server.listen(3000, (err) => {
+  server.listen(port, (err) => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    console.log(`> Ready on http://localhost:${port}`)
   })
 })
 .catch((ex) => {
