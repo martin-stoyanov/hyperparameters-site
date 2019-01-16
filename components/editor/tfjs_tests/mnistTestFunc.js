74d0989d5102a1acc598e133bd158d9b1aaebc55
@martin.a.stoyanov <martin.a.stoyanov@gmail.com>
2019-01-16 18:03:12 -0500

    mnist: hpjs choose between conv or dense net

--- components/editor/tfjs_tests/mnistTestFunc.js
+++ components/editor/tfjs_tests/mnistTestFunc.js
@@ -4,12 +4,16 @@ import * as hpjs from 'hyperparameters';
 export default async ({
   data, onEpochEnd, onExperimentBegin, onExperimentEnd,
 }) => {
-  // training function, called by the optimization function
-  // eslint-disable-next-line no-unused-vars
-  async function trainModel({ kernelSize }, { trainData, testData }) {
-    kernelSize = 3;
+  function createDenseModel() {
+    const model = tf.sequential();
+    model.add(tf.layers.flatten({ inputShape: [28, 28, 1] }));
+    model.add(tf.layers.dense({ units: 42, activation: 'relu' }));
+    model.add(tf.layers.dense({ units: 10, activation: 'softmax' }));
+    return model;
+  }
+  function createConvModel() {
     const model = tf.sequential(); // creating a simple model
-
+    const kernelSize = 4;
     model.add(tf.layers.conv2d({
       inputShape: [28, 28, 1], // data is 28*28 px
       kernelSize,
@@ -23,6 +27,21 @@ export default async ({
     model.add(tf.layers.flatten({}));
     model.add(tf.layers.dense({ units: 64, activation: 'relu' }));
     model.add(tf.layers.dense({ units: 10, activation: 'softmax' }));
+    return model;
+  }
+
+  // training function, called by the optimization function
+  // eslint-disable-next-line no-unused-vars
+  async function trainModel({ modelType }, { trainData, testData }) {
+    let model;
+    console.log(modelType);
+    if (modelType === 'ConvNet') {
+      console.log(modelType);
+      model = createConvModel();
+    } else if (modelType === 'DenseNet') {
+      console.log(modelType);
+      model = createDenseModel();
+    }
     model.summary();
     console.log('1');
     model.compile({
@@ -37,7 +56,7 @@ export default async ({
     const h = await model.fit(trainData.xs, trainData.labels, {
       validationData: [testData.xs, testData.labels],
       validationSplit,
-      epochs: 4,
+      epochs: 6,
       callbacks: { onEpochEnd },
     });
     console.log('3');
@@ -46,9 +65,9 @@ export default async ({
   }
 
   // fmin optmization function, retuns the accuracy, history, confusion matrix data, and a STATUS_OK
-  async function modelOpt({ kernelSize }, { trainData, testData }) {
+  async function modelOpt({ modelType }, { trainData, testData }) {
     // eslint-disable-next-line no-unused-vars
-    const { h, model } = await trainModel({ kernelSize }, { trainData, testData });
+    const { h, model } = await trainModel({ modelType }, { trainData, testData });
 
     return {
       accuracy: h.history.acc[h.history.acc.length - 1],
@@ -60,7 +79,7 @@ export default async ({
   // hyperparameters search space
   // kernelSize is an integer value from 2 to 5 with a step of 1
   const space = {
-    kernelSize: hpjs.quniform(2, 5, 1),
+    modelType: hpjs.choice(['ConvNet', 'DenseNet']),
   };
 
   // Getting data to train, using the tensorflowjs mnist example's data
@@ -72,7 +91,7 @@ export default async ({
   console.log(testData);
 
   return hpjs.fmin(
-    modelOpt, space, hpjs.search.randomSearch, 3,
+    modelOpt, space, hpjs.search.randomSearch, 5,
     {
       rng: new hpjs.RandomState(54321),
       trainData,
