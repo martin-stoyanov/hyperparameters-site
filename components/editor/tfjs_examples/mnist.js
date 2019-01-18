0de2c1addbd04e03058816deebdf8c2bfe62d3a9
@martin.a.stoyanov <martin.a.stoyanov@gmail.com>
2019-01-18 15:48:56 -0500

    mnist: added validation split to search space

--- components/editor/tfjs_examples/mnist.js
+++ components/editor/tfjs_examples/mnist.js
@@ -1,79 +1,87 @@
 export default
 `
+function createDenseModel() {
+  const model = tf.sequential();
+  model.add(tf.layers.flatten({ inputShape: [28, 28, 1] }));
+  model.add(tf.layers.dense({ units: 42, activation: 'relu' }));
+  model.add(tf.layers.dense({ units: 10, activation: 'softmax' }));
+  return model;
+}
+function createConvModel() {
+  const model = tf.sequential(); // creating a simple model
+  const kernelSize = 4;
+  model.add(tf.layers.conv2d({
+    inputShape: [28, 28, 1], // data is 28*28 px
+    kernelSize,
+    filters: 16,
+    activation: 'relu',
+  }));
+  model.add(tf.layers.maxPooling2d({ poolSize: 2, strides: 2 }));
+  model.add(tf.layers.conv2d({ kernelSize, filters: 32, activation: 'relu' }));
+  model.add(tf.layers.maxPooling2d({ poolSize: 2, strides: 2 }));
+  model.add(tf.layers.conv2d({ kernelSize, filters: 32, activation: 'relu' }));
+  model.add(tf.layers.flatten({}));
+  model.add(tf.layers.dense({ units: 64, activation: 'relu' }));
+  model.add(tf.layers.dense({ units: 10, activation: 'softmax' }));
+  return model;
+}
+
 // training function, called by the optimization function
+// eslint-disable-next-line no-unused-vars
+async function trainModel({ modelType, validationSplit }, { trainData, testData }) {
+  let model;
+  if (modelType === 'ConvNet') {
+    model = createConvModel();
+  } else if (modelType === 'DenseNet') {
+    model = createDenseModel();
+  }
+  model.summary();
+  model.compile({
+    optimizer: 'rmsprop',
+    loss: 'categoricalCrossentropy',
+    metrics: ['accuracy'],
+  });
+
+  // Train the model using the data.
+  const h = await model.fit(trainData.xs, trainData.labels, {
+    validationData: [testData.xs, testData.labels],
+    validationSplit,
+    epochs: 4,
+    callbacks: { onEpochEnd },
+  });
+  return { model, h };
+}
+
+// fmin optmization function, retuns the accuracy, history, and a STATUS_OK
+async function modelOpt({ modelType, validationSplit }, { trainData, testData }) {
   // eslint-disable-next-line no-unused-vars
-  async function trainModel({ kernelSize }, { trainData, testData }) {
-    kernelSize = 3;
-    const model = tf.sequential(); // creating a simple model
+  const { h, model } = await trainModel({ modelType, validationSplit }, { trainData, testData });
 
-    model.add(tf.layers.conv2d({
-      inputShape: [28, 28, 1], // data is 28*28 px
-      kernelSize,
-      filters: 16,
-      activation: 'relu',
-    }));
-    model.add(tf.layers.maxPooling2d({ poolSize: 2, strides: 2 }));
-    model.add(tf.layers.conv2d({ kernelSize, filters: 32, activation: 'relu' }));
-    model.add(tf.layers.maxPooling2d({ poolSize: 2, strides: 2 }));
-    model.add(tf.layers.conv2d({ kernelSize, filters: 32, activation: 'relu' }));
-    model.add(tf.layers.flatten({}));
-    model.add(tf.layers.dense({ units: 64, activation: 'relu' }));
-    model.add(tf.layers.dense({ units: 10, activation: 'softmax' }));
-    model.summary();
-    console.log('1');
-    model.compile({
-      optimizer: 'rmsprop',
-      loss: 'categoricalCrossentropy',
-      metrics: ['accuracy'],
-    });
-    console.log('2');
-    const validationSplit = 0.15;
-
-    // Train the model using the data.
-    const h = await model.fit(trainData.xs, trainData.labels, {
-      validationData: [testData.xs, testData.labels],
-      validationSplit,
-      epochs: 4,
-      callbacks: { onEpochEnd },
-    });
-    console.log('3');
-    console.log(h);
-    return { model, h };
-  }
-
-  // fmin optmization function, retuns the accuracy, history, confusion matrix data, and a STATUS_OK
-  async function modelOpt({ kernelSize }, { trainData, testData }) {
-    // eslint-disable-next-line no-unused-vars
-    const { h, model } = await trainModel({ kernelSize }, { trainData, testData });
-
-    return {
-      accuracy: h.history.acc[h.history.acc.length - 1],
-      history: h.history,
-      status: hpjs.STATUS_OK,
-    };
-  }
-
-  // hyperparameters search space
-  // kernelSize is an integer value from 2 to 5 with a step of 1
-  const space = {
-    kernelSize: hpjs.quniform(2, 5, 1),
+  return {
+    accuracy: h.history.acc[h.history.acc.length - 1],
+    history: h.history,
+    status: hpjs.STATUS_OK,
   };
+}
 
-  // Getting data to train, using the tensorflowjs mnist example's data
-  console.log(data);
-  const trainData = data.getTrainData();
-  const testData = data.getTestData();
+// hyperparameters search space
+// modelType is a random string
+// validationSplit is a random # from 0.1-0.25
+const space = {
+  modelType: hpjs.choice(['ConvNet', 'DenseNet']),
+  validationSplit: hpjs.uniform(0.1, 0.25),
+};
 
-  console.log(trainData);
-  console.log(testData);
+// Getting data to train, using the tensorflowjs mnist example's data
+const trainData = data.getTrainData();
+const testData = data.getTestData();
 
-  return hpjs.fmin(
-    modelOpt, space, hpjs.search.randomSearch, 3,
-    {
-      rng: new hpjs.RandomState(54321),
-      trainData,
-      testData,
-      callbacks: { onExperimentBegin, onExperimentEnd },
-    }
-  );
-`;
+return hpjs.fmin(
+  modelOpt, space, hpjs.search.randomSearch, 5,
+  {
+    rng: new hpjs.RandomState(54321),
+    trainData,
+    testData,
+    callbacks: { onExperimentBegin, onExperimentEnd },
+  }
+);`;
--- pages/tensorflow/mnist.js
+++ pages/tensorflow/mnist.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import TensorflowExample from '../../components/TensorflowExample';
-// import mnistTensorflowModel from '../../components/editor/tfjs_examples/mnist';
-import testFunc from '../../components/editor/tfjs_tests/mnistTestFunc';
+import mnistTensorflowModel from '../../components/editor/tfjs_examples/mnist';
+// import testFunc from '../../components/editor/tfjs_tests/mnistTestFunc';
 import { MnistData } from '../../data/mnist';
 
 class MnistPage extends React.Component {
@@ -18,9 +18,9 @@ class MnistPage extends React.Component {
         name='Mnist tensorflow model'
         description='<br /><div>Find best optimizer and number of layers for the the iris dataset.
           </div>'
-        code={undefined}
+        code={mnistTensorflowModel}
         data={data}
-        testFunc={testFunc}
+        testFunc={undefined}
       />
     );
   }
