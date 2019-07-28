21352237bc5aced0f27ac95e02dc847e4df6fd8d
@martin.a.stoyanov <martin.a.stoyanov@gmail.com>
2019-07-28 17:42:10 -0400

    fixed linter errors

--- server.js
+++ server.js
@@ -1,29 +1,27 @@
-const express = require('express')
-const nextjs = require('next')
-const compression = require('compression')
-    
+const express = require('express');
+const nextjs = require('next');
+const compression = require('compression');
+
 const port = parseInt(process.env.PORT, 10) || 3000;
-const dev = process.env.NODE_ENV !== 'production'
-const app = nextjs({ dev })
-const handle = app.getRequestHandler()
-    
+const dev = process.env.NODE_ENV !== 'production';
+const app = nextjs({ dev });
+const handle = app.getRequestHandler();
+
 app.prepare()
-.then(() => {
-  const server = express()
-  if (!dev) {
-    server.use(compression({ threshold: 0 }));
-  }
-    
-  server.get('*', (req, res) => {
-    return handle(req, res)
+  .then(() => {
+    const server = express();
+    if (!dev) {
+      server.use(compression({ threshold: 0 }));
+    }
+
+    server.get('*', (req, res) => handle(req, res));
+
+    server.listen(port, (err) => {
+      if (err) throw err;
+      console.log(`> Ready on http://localhost:${port}`);
+    });
   })
-    
-  server.listen(port, (err) => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
-  })
-})
-.catch((ex) => {
-  console.error(ex.stack)
-  process.exit(1)
-})
+  .catch((ex) => {
+    console.error(ex.stack);
+    process.exit(1);
+  });
