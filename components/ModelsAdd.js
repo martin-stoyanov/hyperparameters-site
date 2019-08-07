352fe7fa193b0b6afcf44b98f5edfccee6f14589
@martin.a.stoyanov <martin.a.stoyanov@gmail.com>
2019-08-07 14:12:41 -0400

    adding template model to database

--- components/ModelsAdd.js
+++ components/ModelsAdd.js
@@ -3,32 +3,42 @@ import gql from 'graphql-tag';
 import { Button, Box, Text } from 'grommet';
 // import ErrorMessage from './ErrorMessage';
 
-function modelAdd(client, { name, trials }) {
+function modelAdd(client, {
+  name, trials, parameters, parameterValues,
+}) {
   client.mutate({
     mutation: gql`
-      mutation addModel($name: String!, $trials: [TrialInputType] ) {
-          addModel(name: $name, trials: $trials) {
-            hpjsModel{
-              name
-            }
+      mutation addModel($name: String!, $trials: [TrialInputType],
+        $parameters: [ParameterInputType], $parameterValues: [ParameterValueInputType] ) {
+          addModel(name: $name, trials: $trials, parameters: $parameters, 
+            parameterValues: $parameterValues) {
+              hpjsModel{
+                name
+              }
           }
       }
     `,
     variables: {
       name,
       trials,
+      parameters,
+      parameterValues,
     },
   });
 }
 
-export default function AddModel({ name, trials }) {
+export default function AddModel({
+  name, trials, parameters, parameterValues,
+}) {
   return (
     <ApolloConsumer>
       {client => (
         <Button
           primary={true}
           hoverIndicator='background'
-          onClick={() => modelAdd(client, { name, trials })}
+          onClick={() => modelAdd(client, {
+ name, trials, parameters, parameterValues,
+})}
         >
           <Box pad={{ horizontal: 'small', vertical: 'xsmall' }}><Text>click</Text></Box>
         </Button>
