057aa91ca30f6eb01e482de722943e6425bf00cf
@martin.a.stoyanov <martin.a.stoyanov@gmail.com>
2019-08-20 20:30:10 -0400

    table of best performing trials on main page

--- components/ModelsList.js
+++ components/ModelsList.js
@@ -1,5 +1,6 @@
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
+import { PagingTable } from 'grommet-controls';
 // import ErrorMessage from './ErrorMessage';
 
 
@@ -9,6 +10,12 @@ export const allModelsQuery = gql`
       name
       trials{
         trial
+        accuracy
+      }parameters{
+        name
+        parameterValue{
+          value
+        }
       }
     }
   }
@@ -21,7 +28,6 @@ export default function ModelsList() {
       {({
  loading, error, data,
 }) => {
-        console.log(data);
         if (error) {
           console.log(error);
           return <div>error</div>;
@@ -30,17 +36,60 @@ export default function ModelsList() {
         if (loading) return <div>Loading</div>;
         if (!data) return null;
         const { allModels } = data;
-        console.log(data);
+        console.log(allModels);
+
+        const bestTrials = allModels.map((model) => {
+          const bestTrialParameters = {
+            'name': model.name,
+            'accuracy': 0,
+            'hyperparameters': {},
+          };
+          let bestTrial;
+          let bestAccuracy = 0;
+          model.trials.forEach((trial) => {
+            if (trial.accuracy > bestAccuracy) {
+              bestAccuracy = trial.accuracy;
+              bestTrialParameters.accuracy = bestAccuracy;
+              bestTrial = trial.trial;
+            }
+            // console.log('aa');
+            // console.log(trial);
+          });
+          model.parameters.forEach((trial) => {
+            // bestTrialParameters.hyperparameters.push(
+            //   {trial.name = trial.parameterValue[bestTrial]
+            //   );
+            bestTrialParameters.hyperparameters[trial.name] = trial.parameterValue[bestTrial].value;
+          });
+          // return bestTrialParameters;
+          console.log(bestTrialParameters);
+          return (
+              {
+                name: bestTrialParameters.name,
+                accuracy: bestTrialParameters.accuracy.toFixed(4),
+                hp: JSON.stringify(bestTrialParameters.hyperparameters),
+              }
+            );
+        });
+        console.log(bestTrials);
+
         return (
           <section>
-            <ul>
-              {allModels.map(model => (
-                <li>
-                  {model.name}
-                  {console.log(model)}
-                </li>
-              ))}
-            </ul>
+            <PagingTable
+              columns={[
+                 {
+                   Header: 'Model Name',
+                   accessor: 'name',
+                 }, {
+                   Header: 'Accuracy',
+                   accessor: 'accuracy',
+                 }, {
+                   Header: 'Hyperparameters',
+                   accessor: 'hp',
+                 },
+                 ]}
+              data={bestTrials}
+            />
           </section>
         );
       }}
